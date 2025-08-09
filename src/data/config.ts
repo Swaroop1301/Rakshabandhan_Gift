13c91ee45f0b4ea6aa495fd81c1c74fdc2e9454f
Swaroop1301 <161961051+Swaroop1301@users.noreply.github.com>
2025-08-09 10:02:41 +0530

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = "Memories of My Sister";
-export const SISTER_NAME = "Diya"; // Edit her name here
+export const SISTER_NAME = "Lipi Didi!"; // Edit her name here
 export const UNLOCK_KEY = "rb_unlocked";
